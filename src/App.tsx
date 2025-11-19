d7242190391f3047d3613455628907e78f7c9e94
cesar.dileonardo <cesar.dileonardo@dataiq.com.ar>
2025-11-19 15:47:24 -0300

    se implementa Google Analytics

--- src/App.tsx
+++ src/App.tsx
@@ -10,9 +10,19 @@ import ScrollToTop from "@/components/scroll/ScrollToTop";
 import ToastProvider, { showToast } from "@/components/Toast/ToastProvider";
 import { useStoreStatus } from "@/context/StoreStatusContext";
 import { useEffect } from "react";
-
+import { analytics } from "@/config/firebase"; // ajusta la ruta según tu estructura
+import { logEvent } from "firebase/analytics";
+import { useLocation } from "react-router-dom";
 function App() {
   const { isStoreOpen, closedMessage } = useStoreStatus();
+  const location = useLocation();
+
+  useEffect(() => {
+    logEvent(analytics, 'page_view', {
+      page_path: location.pathname,
+      page_title: document.title
+    });
+  }, [location]);
 
   useEffect(() => {
     if (!isStoreOpen && closedMessage) {
